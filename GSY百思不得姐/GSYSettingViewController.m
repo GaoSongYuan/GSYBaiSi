79e67d8d41661258ece6eb8b48eef0074d2f335b
Song <784341699@qq.com>
2017-09-19 10:10:57 +0800

    显示缓存
    显示缓存到cell上

--- GSY百思不得姐/GSYSettingViewController.m
+++ GSY百思不得姐/GSYSettingViewController.m
@@ -25,10 +25,12 @@
     self.view.backgroundColor = GSYCommonBgColor;
     self.navigationItem.title = @"设置";
     
+//    NSLog(@"%@",NSHomeDirectory());
+    
     // 利用SDWebImage计算缓存大小
 //    NSLog(@"%zd",[SDImageCache sharedImageCache].getSize);
     
-    NSLog(@"%zd",@"/Users/song/Desktop/ps12-21/jc1502031_2a.jpg".fileSize);
+//    NSLog(@"%zd",@"/Users/song/Desktop/ps12-21/jc1502031_2a.jpg".fileSize);
 }
 
 #pragma mark - 数据源方法
@@ -53,8 +55,38 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
     }
     
-    cell.textLabel.text = @"清除缓存";
-    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator; // 箭头
+//    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator; // 箭头
+    
+    // 设置cell右边的指示器（用来说明正在处理一些事情）
+    UIActivityIndicatorView *loadingView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    [loadingView startAnimating];
+    cell.accessoryView = loadingView;
+    
+    // 设置cell默认的文字
+    cell.textLabel.text = @"清除缓存(正在计算缓存大小...)";
+    
+    // 获得缓存文件夹路径
+    // 在子线程计算缓存大小
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+//        NSString *cachesPath = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES).lastObject;
+//        NSString *dirpath = [cachesPath stringByAppendingPathComponent:@"default"];
+        unsigned long long size = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES).lastObject stringByAppendingPathComponent:@"MP3"].fileSize;
+        size += [SDImageCache sharedImageCache].getSize;
+        
+        // 生成文字
+        NSString *text = [NSString stringWithFormat:@"清除缓存(%zdB)",size];
+    
+        // 回到主线程设置文字
+        dispatch_async(dispatch_get_main_queue(), ^{
+            // 设置文字
+            cell.textLabel.text = text;
+            // 清空右边的指示器
+            cell.accessoryView = nil;
+            // 设置右边箭头
+            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator; // 箭头
+        });
+    });
+    
     return cell;
 }
 
