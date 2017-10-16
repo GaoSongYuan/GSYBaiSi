6dd91a2d3db5297d9543af8d429d96d07abc113a
Song <784341699@qq.com>
2017-10-16 20:17:24 +0800

    加载和显示帖子数据
    加载和显示帖子数据-

--- GSY百思不得姐/GSYAllViewController.m
+++ GSY百思不得姐/GSYAllViewController.m
@@ -8,8 +8,15 @@
 //  全部
 
 #import "GSYAllViewController.h"
+#import <AFNetworking.h>
+#import "XMGTopic.h"
+#import <MJExtension.h>
+#import <UIImageView+WebCache.h>
 
 @interface GSYAllViewController ()
+
+/** 所有的帖子数据 **/
+@property(nonatomic,strong) NSArray<XMGTopic *> *topics;
 
 @end
 
@@ -23,27 +30,64 @@
     // tableView的调整
     self.tableView.contentInset = UIEdgeInsetsMake(64+35, 0, 49, 0);
     self.tableView.scrollIndicatorInsets = self.tableView.contentInset; // 滚动条
+    
+    [self loadNewTopics];
 }
 
+#pragma mark - 加载最新的帖子数据
+-(void)loadNewTopics {
+    
+    // 参数
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"a"] = @"list";
+    params[@"c"] = @"data";
+    
+    // 请求 其中 responseObject是plist文件中的最大的字典
+    [[AFHTTPSessionManager manager] GET:@"http://api.budejie.com/api/api_open.php" parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        // 一个数组，里面放着模型
+        // 将list这个字典数组 转成 装着模型的模型数组
+        NSArray *topics = [XMGTopic mj_objectArrayWithKeyValuesArray:responseObject[@"list"]]; // 从responseObject这个大的字典里面，通过key-list取出
+        self.topics = topics;
+        
+        // 刷新表格
+        [self.tableView reloadData];
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"请求失败 - %@",error);
+    }];
+}
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 2;
-}
+//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+//    return 2;
+//}
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 20;
+    // 服务器返回多少个帖子，这里就有多少行cell
+    return self.topics.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    // 1.确定重用标识
     static NSString *ID = @"cell";
+    // 2.从缓存池中取
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    // 3.如果空就手动创建
     if (!cell) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:ID];
         cell.backgroundColor = GSYRandomColor;
     }
-    cell.textLabel.text = [NSString stringWithFormat:@"%@ - %zd",[self class],indexPath.row];
+    
+    // 4.显示数据
+    XMGTopic *topic = self.topics[indexPath.row]; // 取出模型
+    cell.textLabel.text = topic.name;
+    cell.detailTextLabel.text = topic.text;
+    [cell.imageView sd_setImageWithURL:[NSURL URLWithString:topic.profile_image] placeholderImage:[UIImage imageNamed:@"defaultUserIcon"]];
+    
+    
+//    cell.textLabel.text = [NSString stringWithFormat:@"%@ - %zd",[self class],indexPath.row];
     return cell;
 }
 
